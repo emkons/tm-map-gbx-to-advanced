0c5a77b7041c2e30de14553d3bc5f15bf5129a47
Emils Melders <emkons3@gmail.com>
2022-05-28 02:36:04 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using GBX.NET;
+using GBX.NET.Engines.Game;
+using System;
+using System.IO;
+
+foreach (var fileName in args)
+{
+    var node = GameBox.ParseNode(fileName);
+
+    if (node is CGameCtnChallenge map)
+    {
+        map.Editor = CGameCtnChallenge.EditorMode.Advanced;
+
+        var savePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path.GetFileName(fileName));
+        map.Save(savePath);
+    }
+}
